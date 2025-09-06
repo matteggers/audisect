dc0191acc6464986cfba631aaeaf0f60b4e7de04
matteggers <94013259+matteggers@users.noreply.github.com>
2025-09-06 00:28:38 -0400

    Complied with safety recommendation from sonarqube

--- src/audisect/file.py
+++ src/audisect/file.py
@@ -1,10 +1,11 @@
 from pathlib import Path
+from typing import Optional
 
 class File:
     def __init__(self, path: Path):
         self.path: Path = Path(path)
-        self.txt_path: Path = None
-        self.csv_path: Path = None
+        self.txt_path: Optional[Path] = None
+        self.csv_path: Optional[Path] = None
         self.contents: str = ""
         self.is_transcribed = False
         self.is_analyze = False
